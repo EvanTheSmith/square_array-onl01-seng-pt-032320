114c6c935df9931ac478099e41383f3b7975642c
Evan Smith <professionalemail@zoho.com>
2020-03-30 19:27:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   square_out=[]
-  array.each do |square|
-    square_out << square ** 2
+  array.each do |square_in|
+    square_out << square_in ** 2
   end
   square_out
 end
